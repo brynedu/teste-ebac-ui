67a9b6fc6f1c99f37d8f2d90376c297633144bcc
Brayan Eduardo <brayan10_e@hotmail.com>
2024-09-04 10:08:33 -0300

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types= "cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('teste1924@teste.com')
+        cy.get('#password').type('teste1924')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, teste1924 (não é teste1924? Sair)')
+        })
+    
+})
+
